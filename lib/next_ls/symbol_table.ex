d6371e4f35ea7ce31f933ad6f128554d63f87af5
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-07-03 00:04:09 -0400

    fixup! feat(definition): local function

--- lib/next_ls/symbol_table.ex
+++ lib/next_ls/symbol_table.ex
@@ -41,7 +41,7 @@ defmodule NextLS.SymbolTable do
 
     {:ok, name} =
       :dets.open_file(:symbol_table,
-        file: Path.join(path, "symbol_table.dets") |> String.to_charlist() |> dbg(),
+        file: Path.join(path, "symbol_table.dets") |> String.to_charlist() |> IO.inspect(),
         type: :duplicate_bag
       )
 
