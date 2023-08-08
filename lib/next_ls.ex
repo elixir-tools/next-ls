6ff4c17d631ad37be4903297873ca6ee7a70a38d
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-08-07 22:45:34 -0400

    fix(references): ignore references to elixir source code
    I think these lines are recorded as references when macros in the elixir
    standard library are expanded.

--- lib/next_ls.ex
+++ lib/next_ls.ex
@@ -194,6 +194,7 @@ defmodule NextLS do
                   WHERE refs.identifier = ?
                     AND refs.type = ?
                     AND refs.module = ?
+                    AND NOT like('/home/runner/work/elixir/%', refs.file)
                   """,
                   [function, "function", module]
                 )
@@ -206,6 +207,7 @@ defmodule NextLS do
                   FROM "references" as refs
                   WHERE refs.module = ?
                     and refs.type = ?
+                    AND NOT like('/home/runner/work/elixir/%', refs.file)
                   """,
                   [module, "alias"]
                 )
@@ -712,5 +714,6 @@ defmodule NextLS do
         end
     end
   end
+
   defp clamp(line), do: max(line, 0)
 end
