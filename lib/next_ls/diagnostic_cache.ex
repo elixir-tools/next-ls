ebb45a4acc397ebc2090efb817d48f06f781c6bc
Nikola Jichev <njichev@gmail.com>
2023-12-20 18:00:03 +0200

    refactor: diagnostic cache to an ets table

--- lib/next_ls/diagnostic_cache.ex
+++ lib/next_ls/diagnostic_cache.ex
@@ -1,33 +1,69 @@
 defmodule NextLS.DiagnosticCache do
-  # TODO: this should be an ETS table
   @moduledoc false
-  use Agent
+  use GenServer
 
   def start_link(opts) do
-    Agent.start_link(fn -> Map.new() end, Keyword.take(opts, [:name]))
+    opts = Keyword.take(opts, [:name])
+    GenServer.start_link(__MODULE__, opts, opts)
+  end
+
+  def init(opts) do
+    name = opts[:name]
+
+    ets =
+      if name do
+        :ets.new(name, [:named_table, :protected, :bag, {:read_concurrency, true}])
+      else
+        :ets.new(:diagnostic_cache, [:protected, :bag, {:read_concurrency, true}])
+      end
+
+    {:ok, %{ets: ets}}
   end
 
   def get(cache) do
-    Agent.get(cache, & &1)
+    cache
+    |> GenServer.call(:all)
+    |> to_map()
+  end
+
+  def get(cache, namespace) do
+    GenServer.call(cache, {:get, namespace})
   end
 
   def put(cache, namespace, filename, diagnostic) do
-    Agent.update(cache, fn cache ->
-      Map.update(cache, namespace, %{filename => [diagnostic]}, fn cache ->
-        Map.update(cache, filename, [diagnostic], fn v ->
-          [diagnostic | v]
-        end)
-      end)
-    end)
+    GenServer.call(cache, {:put, filename, namespace, diagnostic})
   end
 
   def clear(cache, namespace) do
-    Agent.update(cache, fn cache ->
-      Map.update(cache, namespace, %{}, fn cache ->
-        for {k, _} <- cache, into: Map.new() do
-          {k, []}
-        end
-      end)
-    end)
+    GenServer.call(cache, {:clear, namespace})
+  end
+
+  def handle_call(:all, _from, %{ets: ets} = state) do
+    result = :ets.tab2list(ets)
+    {:reply, result, state}
+  end
+
+  def handle_call({:get, namespace}, _from, %{ets: ets} = state) do
+    result = :ets.lookup(ets, namespace)
+    {:reply, result, state}
+  end
+
+  def handle_call({:put, namespace, filename, diagnostic}, _from, %{ets: ets} = state) do
+    :ets.insert(ets, {filename, namespace, diagnostic})
+    {:reply, :ok, state}
+  end
+
+  def handle_call({:clear, namespace}, _from, %{ets: ets} = state) do
+    :ets.delete(ets, namespace)
+    {:reply, :ok, state}
+  end
+
+  defp to_map(list) do
+    for {namespace, filename, diagnostic} <- list, reduce: %{} do
+      d ->
+        Map.update(d, namespace, %{filename => [diagnostic]}, fn value ->
+          Map.update(value, filename, [diagnostic], fn diagnostics -> [diagnostic | diagnostics] end)
+        end)
+    end
   end
 end
