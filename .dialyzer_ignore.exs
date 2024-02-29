b7cdf49ce65a5c48337b3aa70f5e333a36fca3ac
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-02-29 17:22:14 -0500

    fix: properly initiate progress notification

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,5 @@
 [
   {"lib/next_ls/lsp_supervisor.ex", :exact_eq},
-  {"lib/next_ls/runtime.ex", :exact_eq}
+  {"lib/next_ls/runtime.ex", :exact_eq},
+  {"lib/next_ls/progress.ex", :pattern_match}
 ]
