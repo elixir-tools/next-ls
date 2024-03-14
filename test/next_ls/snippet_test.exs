0c48c409e68c5321165e1d3724f18a622e2a6fd9
Luca Cervello <luca.cervello@gmail.com>
2024-03-14 14:01:48 +0100

    chore: add more test for defmodule snippet

---
+++ test/next_ls/snippet_test.exs
@@ -0,0 +1,37 @@
+defmodule NextLS.SnippetTest do
+  use ExUnit.Case, async: true
+
+  alias NextLS.Snippet
+
+  describe "defmodule snippet" do
+    test "simple module" do
+      assert %{insert_text: "defmodule ${1:Foo} do\n  $0\nend\n", insert_text_format: 2, kind: 15} ==
+               Snippet.get("defmodule/2", nil, uri: "file:///my_proj/lib/foo.ex")
+    end
+
+    test "nested module" do
+      assert %{insert_text: "defmodule ${1:Foo.Bar.Baz} do\n  $0\nend\n", insert_text_format: 2, kind: 15} ==
+               Snippet.get("defmodule/2", nil, uri: "file:///my_proj/lib/foo/bar/baz.ex")
+    end
+
+    test "test module" do
+      assert %{insert_text: "defmodule ${1:FooTest} do\n  $0\nend\n", insert_text_format: 2, kind: 15} ==
+               Snippet.get("defmodule/2", nil, uri: "file:///my_proj/test/foo_test.exs")
+    end
+
+    test "support test module" do
+      assert %{insert_text: "defmodule ${1:Foo} do\n  $0\nend\n", insert_text_format: 2, kind: 15} ==
+               Snippet.get("defmodule/2", nil, uri: "file:///my_proj/test/support/foo.ex")
+    end
+
+    test "module outside canonical folders" do
+      assert %{insert_text: "defmodule ${1:Foo} do\n  $0\nend\n", insert_text_format: 2, kind: 15} ==
+               Snippet.get("defmodule/2", nil, uri: "file:///my_proj/foo.ex")
+    end
+
+    test "without uri" do
+      assert %{insert_text: "defmodule ${1:ModuleName} do\n  $0\nend\n", insert_text_format: 2, kind: 15} ==
+               Snippet.get("defmodule/2", nil)
+    end
+  end
+end
