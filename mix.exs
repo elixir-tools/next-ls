64fe2b3037f4e9f2b12ca40d007f609b88ddcf95
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-03-26 21:35:43 -0400

    fix: update sourceror

--- mix.exs
+++ mix.exs
@@ -64,9 +64,7 @@ defmodule NextLS.MixProject do
       {:req, "~> 0.3"},
       {:schematic, "~> 0.2"},
       {:spitfire, github: "elixir-tools/spitfire"},
-      # {:sourceror, "~> 1.0"},
-      # {:sourceror, path: "../sourceror"},
-      {:sourceror, github: "mhanberg/sourceror", branch: "do-end-blocks-with-eoe"},
+      {:sourceror, "~> 1.0"},
 
       {:opentelemetry, "~> 1.3"},
       {:opentelemetry_api, "~> 1.2"},
