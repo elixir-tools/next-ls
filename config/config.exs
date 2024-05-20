656050f1a6a5b82b4598ad0651afd5caf6c9e51c
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-05-20 09:36:38 -0400

    chore: justfile

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,12 @@ import Config
 
 config :next_ls, :indexing_timeout, 100
 
+case System.get_env("NEXTLS_RELEASE_MODE", "plain") do
+  "burrito" ->
+    config :next_ls, arg_parser: {Burrito.Util.Args, :get_arguments, []}
+
+  "plain" ->
+    config :next_ls, arg_parser: {System, :argv, []}
+end
+
 import_config "#{config_env()}.exs"
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,13 +1,5 @@
 import Config
 
-case System.get_env("NEXTLS_RELEASE_MODE", "plain") do
-  "burrito" ->
-    config :next_ls, arg_parser: {Burrito.Util.Args, :get_arguments, []}
-
-  "plain" ->
-    config :next_ls, arg_parser: {System, :argv, []}
-end
-
 if System.get_env("NEXTLS_OTEL") == "1" do
   config :next_ls,
     otel: true
