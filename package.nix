7086c78c14299fa16e84a26a7590f889fed8a8b9
Adam C. Stephens <2071575+adamcstephens@users.noreply.github.com>
2024-07-03 11:10:12 -0400

    chore(nix): updates to use nixpkgs elixir 1.17 (#525)
    * chore(nix): flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/57d6973abba7ea108bac64ae7629e7431e0199b6' (2024-06-12)
      → 'github:NixOS/nixpkgs/a71e967ef3694799d0c418c98332f7ff4cc5f6af' (2024-06-22)
    
    * chore(nix): use pkgs.elixir_1_17 and split package file

---
+++ package.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  beamPackages,
+  elixir,
+}:
+
+beamPackages.mixRelease rec {
+  pname = "next-ls";
+  src = ./.;
+  mixEnv = "prod";
+  removeCookie = false;
+  version = "0.23.0"; # x-release-please-version
+
+  inherit elixir;
+  inherit (beamPackages) erlang;
+
+  mixFodDeps = beamPackages.fetchMixDeps {
+    inherit src version elixir;
+    pname = "next-ls-deps";
+    hash = "sha256-4Rt5Q0fX+fbncvxyXdpIhgEvn9VYX/QDxDdnbanT21Q=";
+    mixEnv = "prod";
+  };
+
+  installPhase = ''
+    mix release --no-deps-check --path $out plain
+    echo "$out/bin/plain eval \"System.no_halt(true); Application.ensure_all_started(:next_ls)\" \"\$@\"" > "$out/bin/nextls"
+    chmod +x "$out/bin/nextls"
+  '';
+
+  meta = with lib; {
+    license = licenses.mit;
+    homepage = "https://www.elixir-tools.dev/next-ls/";
+    description = "The language server for Elixir that just works";
+    mainProgram = "nextls";
+  };
+}
