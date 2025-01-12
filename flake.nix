8816f6abce137de5495534110141ae11d8bcda7b
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-01-12 16:20:41 -0500

    chore(nix): update flake

--- flake.nix
+++ flake.nix
@@ -25,6 +25,11 @@
         zpkgs = zigpkgs.legacyPackages.${system};
         beamPackages = pkgs.beam_minimal.packages.erlang_27;
         elixir = beamPackages.elixir_1_17;
+        # example of overriding elixir with whatever you want
+        # elixir = beamPackages.elixir_1_18.override {
+        #   rev = "f16fb5aa8162794616a738fc6e84bfcdf9892cff";
+        #   sha256 = "sha256-UYWsmih+0z+4tdPhxl2zf+4gUNEgRJR4yyvxVBOgJdQ=";
+        # };
       in
         f {inherit system pkgs zpkgs beamPackages elixir;});
 
@@ -42,7 +47,7 @@
       elixir,
       ...
     }: {
-      default = pkgs.callPackage ./package.nix { inherit beamPackages elixir; };
+      default = pkgs.callPackage ./package.nix {inherit beamPackages elixir;};
     });
 
     devShells = forAllSystems ({
